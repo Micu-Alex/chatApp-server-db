929ed3a223b5d10c7707871536626288f81bd088
Micu-Alex <alexmicu389@gmail.com>
2023-12-04 20:37:34 +0200

    first backend commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const express = require('express');
+const app = express()
+
+mongoose.connect("mongodb://127.0.0.1/whatsapp-copy")
+    .then(() => console.log(`connected to MongoDb`))
+    .catch(err => console.log(err))
+
+
+app.get("/", (req, res) => {
+    res.send("hello world")
+})
+
+app.listen(3000, () => console.log("app listening on port 3000"))
