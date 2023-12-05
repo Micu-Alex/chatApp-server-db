bbcb094d1852637c28b360d917ab27ed78892892
Micu-Alex <alexmicu389@gmail.com>
2023-12-05 23:56:23 +0200

    instaled config

--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Joi = require("joi")
+const jwt = require("jsonwebtoken")
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -22,6 +23,12 @@ const userSchema = new mongoose.Schema({
         maxlenght: 80,
     },
 });
+
+
+userSchema.methods.generateAuthToken = function() {
+    const token = jwt.sign({_id: this._id,}, config.get("jwtPrivateKey"))
+    return token
+}
 
 const User = mongoose.model('user', userSchema);
 
