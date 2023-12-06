70c800f1c2ff10560ec796bb3d25a87f338bf1c4
Micu-Alex <alexmicu389@gmail.com>
2023-12-06 21:06:22 +0200

    added password encryption

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,8 @@
 const {User, validate} = require("../models/user")
 const express = require('express');
+const bcrypt = require("bcrypt")
 
 const router = express.Router()
-
 
 router.get("/", async (req, res) => {
     const users = await User.find().sort("name")
@@ -17,19 +17,17 @@ router.post("/", async (req, res) => {
     let user = await User.findOne({email: req.body.email})
     if (user) return res.status(400).send("User already registered")
 
-    
-
     user = new User({
         name: req.body.name,
         email: req.body.email,
         password: req.body.password
     })
+    const salt = await bcrypt.genSalt(10)
+    user.password = await bcrypt.hash(user.password, salt)
     await user.save()
 
     const token = user.generateAuthToken()
     res.header("x-auth-token", token).send({ _id: user._id, name: user.name })
-
-    res.send(user)
 })
 
 
