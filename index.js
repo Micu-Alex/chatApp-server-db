c211a04db15a29b02bb5332a147bfaa6bc8c7fa9
Micu-Alex <alexmicu389@gmail.com>
2023-12-06 20:33:27 +0200

    added env variables

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ const mongoose = require('mongoose');
 const users = require("./routes/users")
 const messages = require("./routes/messages")
 const express = require('express');
+const config = require("config")
 const app = express()
 
 mongoose.connect("mongodb://127.0.0.1/whatsapp-copy")
     .then(() => console.log(`connected to MongoDb`))
     .catch(err => console.log(err))
 
+
+if (!config.get("jwtPrivateKey")) throw new Error("FATAL ERROR: jwtPrivateKey is not defined")
 
 app.use(express.json())
 app.use("/api/users", users)
