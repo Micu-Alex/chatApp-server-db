5020d79677f0107ed412261fe3564b116bbd6744
Micu-Alex <alexmicu389@gmail.com>
2023-12-13 19:44:08 +0200

    fixed trim issue

--- routes/users.js
+++ routes/users.js
@@ -18,9 +18,9 @@ router.post("/", async (req, res) => {
     if (user) return res.status(400).send("User already registered")
 
     user = new User({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password
+        name: req.body.name.trim(),
+        email: req.body.email.trim(),
+        password: req.body.password.trim()
     })
     const salt = await bcrypt.genSalt(10)
     user.password = await bcrypt.hash(user.password, salt)
