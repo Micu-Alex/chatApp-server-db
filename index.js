b1b9f573be154d46089d0890bffe55d01228abf8
Micu-Alex <alexmicu389@gmail.com>
2024-01-01 03:40:32 +0200

    refactored index.js

--- index.js
+++ index.js
@@ -1,28 +1,14 @@
 const express = require('express');
-const config = require("config")
 const { join } = require('node:path');
 const { createServer } = require('node:http')
 const handleSocket = require('./sockets/socketHandler');
 const app = express();
-require("./startup/routes")(app);
-require("./startup/db")();
 const server = createServer(app);
 
+require("./startup/routes")(app);
+require("./startup/db")();
+require("./startup/config")();
 
-
-
-
-
-if (!config.get("jwtPrivateKey")) throw new Error("FATAL ERROR: jwtPrivateKey is not defined")
-
-
-app.get('/', (req, res) => {
-  res.sendFile(join(__dirname, 'index.html'));
-});
-
-app.get('/login', (req, res) => {
-  res.sendFile(join(__dirname, 'login.html'));
-});
 
 handleSocket(server) 
 
