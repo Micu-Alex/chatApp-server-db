16fd5d35a125d5c225a1c418d4f545d70116d166
Micu-Alex <alexmicu389@gmail.com>
2024-01-01 03:44:37 +0200

    reformed cores

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const { join } = require('node:path');
 const { createServer } = require('node:http')
 const handleSocket = require('./sockets/socketHandler');
 const app = express();
@@ -9,9 +8,7 @@ require("./startup/routes")(app);
 require("./startup/db")();
 require("./startup/config")();
 
-
 handleSocket(server) 
-
 
 const port = 3000
 server.listen(port, () => console.log(`app listening on ${port}`))
--- startup/routes.js
+++ startup/routes.js
@@ -1,13 +1,12 @@
 const users = require("../routes/users")
 const auth = require("../routes/auth")
 const express = require('express');
-const cors = require('cors');
+const corsMiddleware = require("../middleware/cores");
 
-const corsOptions = {
-    origin: "http://localhost:5173"
-  }
+
+
 module.exports = (app) => {
-    app.use(cors(corsOptions));
+    app.use(corsMiddleware);
     app.use(express.json())
     app.use("/api/users", users)
     app.use("/api/auth", auth)}
