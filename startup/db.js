7910cffdb881348190c3948efb785953a2efff4b
Micu-Alex <alexmicu389@gmail.com>
2024-01-14 20:49:39 +0200

    updated db conection link

--- startup/db.js
+++ startup/db.js
@@ -3,7 +3,7 @@ const config = require("config")
 
 const mongodbConfig = config.get('mongodb');
 module.exports = () => {
-    mongoose.connect(`mongodb+srv://alexmicu389:${mongodbConfig.password}@cluster0.onjjcr7.mongodb.net/?retryWrites=true&w=majority`)
+    mongoose.connect(`mongodb+srv://alexmicu389:${mongodbConfig.password}@cluster0.onjjcr7.mongodb.net/chatApp?retryWrites=true&w=majority`)
         .then(() => console.log(`connected to MongoDb`))
         .catch(err => console.log(err))
 }
