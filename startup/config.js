5fd6185d8dae61748ca072d944a018922e0f47a1
Micu-Alex <alexmicu389@gmail.com>
2024-01-14 20:41:01 +0200

    updated db to use atlas

--- startup/config.js
+++ startup/config.js
@@ -2,4 +2,5 @@ const config = require("config")
 
 module.exports = () => {
     if (!config.get("jwtPrivateKey")) throw new Error("FATAL ERROR: jwtPrivateKey is not defined")
+    if (!config.get("mongodb.password")) throw new Error("FATAL ERROR: mongodb password is not defined")
 }
--- startup/db.js
+++ startup/db.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
+const config = require("config")
 
+const mongodbConfig = config.get('mongodb');
 module.exports = () => {
-    mongoose.connect("mongodb://127.0.0.1/whatsapp-copy")
+    mongoose.connect(`mongodb+srv://alexmicu389:${mongodbConfig.password}@cluster0.onjjcr7.mongodb.net/?retryWrites=true&w=majority`)
         .then(() => console.log(`connected to MongoDb`))
         .catch(err => console.log(err))
 }
