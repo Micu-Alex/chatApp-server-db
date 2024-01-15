f147617d55c643f352585e01112c1edbf7e117f7
Micu-Alex <alexmicu389@gmail.com>
2024-01-15 22:03:47 +0200

    test 2

--- startup/config.js
+++ startup/config.js
@@ -2,5 +2,5 @@ const config = require("config")
 
 module.exports = () => {
     if (!config.get("jwtPrivateKey")) throw new Error("FATAL ERROR: jwtPrivateKey is not defined")
-    if (!config.get("mongodb.password")) throw new Error("FATAL ERROR: mongodb password is not defined")
+    if (!config.get("mongodb.mongoAdress")) throw new Error("FATAL ERROR: mongodb address is not defined")
 }
--- startup/db.js
+++ startup/db.js
@@ -3,7 +3,8 @@ const config = require("config")
 
 const mongodbConfig = config.get('mongodb');
 module.exports = () => {
-    mongoose.connect(`mongodb+srv://alexmicu389:${mongodbConfig.password}@cluster0.onjjcr7.mongodb.net/chatApp?retryWrites=true&w=majority`)
+
+    mongoose.connect(mongodbConfig.mongoAdress)
         .then(() => console.log(`connected to MongoDb`))
         .catch(err => console.log(err))
 }
